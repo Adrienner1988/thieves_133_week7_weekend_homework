90710f1adcf2fb83e2b231a144d7d4c55dc4131e
Adrienne Daniels <106035434+Adrienner1988@users.noreply.github.com>
2023-11-30 15:16:27 -0500

    added new icons messed around with styles of parsing cleaned up code

--- main.js
+++ main.js
@@ -1,44 +1,44 @@
 //DOM Objects
 const cityName = document.querySelector('.city-name');
-console.log(cityName);
+
 
 //image
 const imageIcon = document.querySelector('#icon');
-console.log(imageIcon);
+
+
 
 // main-temp
 const mainTemp = document.querySelector('#temp');
-console.log(mainTemp);
+
 
 //weather description
 const description = document.querySelector('#description');
-console.log(description);
+
 
 //main-max-high
 const highTemp = document.querySelector('#high');
-console.log(highTemp);
+
 
 //main-max-low
 const lowTemp = document.querySelector('#low');
-console.log(lowTemp);
+
 
 
 
 //Calling API
-const weatherApi = async () => {
-    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?zip=33974,us&appid=a889a853f78d336ee0ec8ca80895b827&units=imperial');
+const weatherApi = async (cityname) => {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=a889a853f78d336ee0ec8ca80895b827&units=imperial`);
     const data = await response.json();
-    console.log(data)
+  
 
     //parsing through the data
-    cityName.textContent = data['name'];
-    imageIcon.textContent = data['weather'][0].icon
+    cityName.textContent = data.name
+    imageIcon.src = data.weather.icon
     mainTemp.textContent = data['main']['temp']
     description.textContent = data['weather'][0].main
     highTemp.textContent = data['main']['temp_max']
     lowTemp.textContent = data['main']['temp_min']
 };
-weatherApi();
 
 
 //grabbing data from the form
